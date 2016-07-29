f18386cae25f165d59a1979204a6a25859edc195
Charlie Farber <berightback42@gmail.com>
2016-07-29 10:14:33 -0700

    mroe checking

--- lib/helix_neural_network.py
+++ lib/helix_neural_network.py
@@ -41,7 +41,7 @@ def predict(test_data, true_labels, batch_size, model, model_file=None):
 
     probs = list(chain(*probs))
     probs = list(zip(test_data, true_labels, probs))
-    print (errors)
+    print (len(errors))
     return errors, probs
 
 
